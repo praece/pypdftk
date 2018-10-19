1d58ccd3395413e82a8507a8fa83a607b085564e
Kevin OBrien <kevin.obrien@praece.com>
2018-10-19 11:12:51 -0700

    Update pypdftk.py

--- pypdftk.py
+++ pypdftk.py
@@ -81,6 +81,7 @@ def fill_form(pdf_path, datas={}, out_file=None, flatten=True):
     finally:
         if handle:
             os.close(handle)
+    os.close(tmp_fdf)
     os.remove(tmp_fdf)
     return out_file
 
